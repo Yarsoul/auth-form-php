be9fc7813c290ed7a03be48d2180832e440004b2
Yaroslav D <yaroooartal@mail.ru>
2022-03-03 23:27:42 +0700

    update password and readme

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php 
 	$host = "127.0.0.1";
 	$data_base = "my_data_form";
-	$user = "root";
-	$pass = "12345678";
+	$user = "your_username";
+	$pass = "your_password";
 
 	$link = mysqli_connect($host, $user, $pass, $data_base) or die("Error_link".mysqli_error($link));
 ?>
